ad4ffb576879afecfeefa7bf4eacbdf08eeb3d49
carlosgadelha <josecarlos.gadelha10@gmail.com>
2022-01-26 22:57:35 -0300

    Botão de finalizar pedido concluido

--- script.js
+++ script.js
@@ -1,40 +1,57 @@
-function selecionar(){
+function selecionar(item, id){
+    if (item != null){
+        document.getElementById(item).style.border = "none";
+        document.getElementById(id).style.border = "5px solid green";
+        
+    }else{
+        document.getElementById(id).style.border = "5px solid green";
+
+    }
+
+    // return item
 
 }
 
 function selecaoPrato(id){
 
-    let selecionado = elementoSelecionado()
-    if (selecionado != null){
-        document.getElementById(selecionado).style.border = "none";
-        document.getElementById(id).style.border = "5px solid green";
-        
-        console.log(selecionado)
-    }else{
-        document.getElementById(id).style.border = "5px solid green";
+    let itemSelecionado = elementoSelecionado("pratos")
+    selecionar(itemSelecionado, id)
+    ativarBotaoPrdido()
 
+}
+
+function selecaoBebida(id){
+
+    let itemSelecionado = elementoSelecionado("bebidas")
+    selecionar(itemSelecionado, id)
+    ativarBotaoPrdido()
+
+}
+
+function selecaoSobremesa(id){
+
+    let itemSelecionado = elementoSelecionado("sobremesas")
+    selecionar(itemSelecionado, id)
+    ativarBotaoPrdido()
+
+}
+
+function ativarBotaoPrdido(){
+
+    let itemSelecionadoPrato = elementoSelecionado("pratos")
+    let itemSelecionadoBebida = elementoSelecionado("bebidas")
+    let itemSelecionadoSobremesa = elementoSelecionado("sobremesas")
+
+    if(itemSelecionadoPrato != null && itemSelecionadoBebida != null && itemSelecionadoSobremesa != null){
+        document.getElementById("pedido").style.backgroundColor = "rgba(50, 183, 47, 1)";
     }
-    
-    // //console.log(selecionado)
-    // if( selecionado != null){
-        
-        
-    //     selecionado = id;
-            
-    //     // if(selecionado != null){
-    //     //     
-    //     // }
-        
-    //     //console.log(selecionado)
-        
-    // }
-
+ 
 }
 
 
 
-function elementoSelecionado(){
-    const produtos = document.getElementById("pratos").children
+function elementoSelecionado(produto){
+    const produtos = document.getElementById(produto).children
     for( let i  = 0; i < produtos.length; i++ ){
         let id = produtos[i].getAttribute("id");
         let borda = document.getElementById(id).getAttribute("style");
@@ -44,26 +61,6 @@ function elementoSelecionado(){
           return id;
         }
     }
-
-    // let lista = produtos.map((element )=> {
-    //     return element[0].getAttribute("id")
-    // });
-
-    
-    // console.log(produtos[0].getAttribute("id").getAttribute("style"))
-    
-       
-    // for( let i  = 0; i < produtos.lenght; i++ ){
-    //     let id = produtos[i].getAttribute("id");
-    //     console.log("id")
-    //     // console.log(document.getElementById(id).getAttribute("style"))
-
-    // //    console.log(produtos[i].getAttribute("border"))
-    // //    if(document.getElementById(id).getAttribute("style") == "5px solid green"){
-    // //        console.log("true");
-    //     //    produtos[i].style.border = "none"
-
-    //    }
 
     }
 
