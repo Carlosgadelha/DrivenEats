cc7ebf99cf662b31c303b691a0e0a923f969cf95
carlosgadelha <josecarlos.gadelha10@gmail.com>
2022-01-31 00:49:18 -0300

    pequenas alteracoes

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// function selecionarItem(item, tipoProduto){
+
+//     const produto = document.querySelector('.'+ tipoProduto + '.selecionado');
+
+//     if (produto != null){
+//         produto.classList.remove('selecionado')
+//     } else{
+//         document.querySelector('.'+ tipoProduto + '.selecionado')
+
+//     }
+
+// }
+
+
 function selecionar(item, id){
+
     if (item != null){
         document.getElementById(item).style.border = "none";
         document.getElementById(id).style.border = "5px solid green";
@@ -9,6 +24,20 @@ function selecionar(item, id){
     }
 
     // return item
+
+}
+
+function elementoSelecionado(produto){
+    const produtos = document.getElementById(produto).children
+    for( let i  = 0; i < produtos.length; i++ ){
+        let id = produtos[i].getAttribute("id");
+        let borda = document.getElementById(id).getAttribute("style");
+
+
+        if( borda === "border: 5px solid green;"){
+          return id;
+        }
+    }
 
 }
 
@@ -37,30 +66,39 @@ function selecaoSobremesa(id){
 }
 
 function ativarBotaoPrdido(){
-
+    let text = ""
     let itemSelecionadoPrato = elementoSelecionado("pratos")
     let itemSelecionadoBebida = elementoSelecionado("bebidas")
     let itemSelecionadoSobremesa = elementoSelecionado("sobremesas")
+    
+    
+    // console.log(preco);
 
     if(itemSelecionadoPrato != null && itemSelecionadoBebida != null && itemSelecionadoSobremesa != null){
         document.getElementById("pedido").style.backgroundColor = "rgba(50, 183, 47, 1)";
+
+        // console.log( document.getElementById(itemSelecionadoPrato ).children[0].children[3].innerHTML)
+
+        let {nomePrato, precoPrato} = obterNomePreco(itemSelecionadoPrato)
+        let {nomeBebida, precoBebida} = obterNomePreco(itemSelecionadoBebida)
+        let {nomeSobremesa, precoSobremesa} = obterNomePreco(itemSelecionadoSobremesa)
+         
+        console.log(nomeBebida)
+        // text = `${nomePrato}   ${precoPrato}  `
+        // console.log(text)
     }
  
 }
 
+function obterNomePreco(itemSelecionado){
+
+    let nome = document.getElementById(itemSelecionado).children[0].children[1].innerHTML
+    
+    let preco = document.getElementById(itemSelecionado).children[0].children[3].innerHTML
+    
+
+    return {nome, preco}
+}
 
 
-function elementoSelecionado(produto){
-    const produtos = document.getElementById(produto).children
-    for( let i  = 0; i < produtos.length; i++ ){
-        let id = produtos[i].getAttribute("id");
-        let borda = document.getElementById(id).getAttribute("style");
-
-
-        if( borda === "border: 5px solid green;"){
-          return id;
-        }
-    }
-
-    }
 
